813f375cb8440bbdd4a869f7f53fe7ea85f3a124
German Leonel Espindola <german.espindola@gmail.com>
2022-07-25 10:48:03 -0300

    Update app.js

--- app.js
+++ app.js
@@ -1,6 +1,9 @@
 const express = include('express');
 const app = express();
 
+app.get('/', (req, res) => {
+  res.send('Hola Mundo');
+})
 
 
 app.listen(300, ()=> console.log("Servidor escuchando en puerto 3000"));
